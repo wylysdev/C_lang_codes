81987fe134990f309d7c5e9f74426f2738a0ef7c
wylysdev <willysangelo9876@gmail.com>
2021-09-10 20:51:05 -0300

    encadeadas

---
+++ estruturas_dados_c/fila/encadeadas/exemplo1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+struct Node {
+    int info;
+    int next;
+};
+
+#define NODE_SIZE 10
+
+typedef struct Node node_t;
+
+
+void alloc_node (){}
+
+void free_node(){}
+
+int main (){
+
+    // criar o balaio de nos disponiveis
+    
+
+    node_t fila[NODE_SIZE];
+    
+
+}
